ca53964a45e853b1c8caf1dd1ffe5816fb20258e
Devendra (Kyouma) <hkyouma07@gmail.com>
2025-01-09 18:19:43 +0530

    odd_even.java has been  uploaded

---
+++ src/odd_even.java
@@ -0,0 +1,16 @@
+public class odd_even {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 10; i++) {
+
+            if ( i%2  != 0){
+                System.out.println(i + " is a not an even number");
+            }
+            else
+                System.out.println(i+ " is an even number");
+        }
+    }
+}
