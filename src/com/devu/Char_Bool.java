3408ca81e98dfd0b16787353058d5f63c21d88a9
Devendra (Kyouma) <hkyouma07@gmail.com>
2025-01-14 22:36:58 +0530

    .\Char_Bool.java uploaded

---
+++ src/com/devu/Char_Bool.java
@@ -0,0 +1,22 @@
+package com.devu;
+
+public class Char_Bool {
+    public static void main(String[] args){
+        System.out.println("Char and Boolean");
+
+        char myChar = 'D';
+        System.out.println(myChar);
+
+        char myChar1 = '\u0044';
+        System.out.println(myChar1);
+
+        char myChar2 = 68;
+        System.out.println(myChar2);
+
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+    }
+}
