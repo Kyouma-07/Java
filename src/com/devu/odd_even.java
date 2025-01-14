2f8256b469d78c4ee2d2d42b72510814e17379c8
Devendra (Kyouma) <hkyouma07@gmail.com>
2025-01-14 11:56:20 +0530

    new change with pacakges

--- src/com/devu/odd_even.java
+++ src/com/devu/odd_even.java
@@ -1,3 +1,5 @@
+
+package com.devu;
 public class odd_even {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
