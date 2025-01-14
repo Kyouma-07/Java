c522f7bb410b5145fb25a9e1368e943c946ca5c2
Devendra (Kyouma) <hkyouma07@gmail.com>
2025-01-14 20:31:19 +0530

    Conversion.java added

--- src/com/devu/Conversion.java
+++ src/com/devu/Conversion.java
@@ -1,4 +1,13 @@
 package com.devu;
 
 public class Conversion {
+    public static void main(String[] args){
+
+        System.out.println("Conversion Program");
+
+        double weightKG = 10;
+
+        double kg_pound = weightKG /0.45359237;
+        System.out.println( kg_pound);
+    }
 }
