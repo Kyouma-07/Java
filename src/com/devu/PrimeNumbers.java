e056d84374c451831f99f0af8526d49ae8d45c54
Devendra (Kyouma) <hkyouma07@gmail.com>
2025-02-04 22:09:37 +0530

    Prime Numbers program have been been uploaded

--- src/com/devu/PrimeNumbers.java
+++ src/com/devu/PrimeNumbers.java
@@ -1,4 +1,24 @@
 package com.devu;
 
+import java.util.Scanner;
+
 public class PrimeNumbers {
+    public static void main(String[] args){
+        System.out.println("Programm to check prime number:");
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        sc.close();
+
+        for( int i = 2 ;i <  num/2 ; i++) {
+            int flag = 0;
+            if( num% i  == 0) {
+                flag++;
+                System.out.println("Not a prime number");
+                break;
+            }
+        }
+        System.out.println("Is a prime number:");
+    }
 }
